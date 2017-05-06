1af49031fb7be0ddaed1cb6c21f18183bd1f827b
Vahid Fazlollahzade <vahid.fazl2000+gitlab@gmail.com>
2017-05-07 00:12:22 +0430

    Published on hex

--- mix.exs
+++ mix.exs
@@ -2,12 +2,19 @@ defmodule Constants.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :constants,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :constants,
+      version: "0.1.0",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Constants",
+      source_url: "https://github.com/vfsoraki/constants"
+    ]
   end
 
   def application do
@@ -15,6 +22,23 @@ defmodule Constants.Mixfile do
   end
 
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
   end
+
+  defp description do
+  """
+  A library to help your easily define constants.
+  """
+  end
+
+  defp package do
+    [
+      name: :constants,
+      files: ["lib", "mix.exs", "README.md",],
+      maintainers: ["Vahid Fazlollahzade"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/vfsoraki/constants"}
+    ]
+  end
+
 end
